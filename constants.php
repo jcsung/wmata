91ab38de2d75db1b8e0b971daeda7f84d51f979d
Jeffrey Sung <jeffrey.sung@t440>
2022-01-13 17:51:23 -0500

    Add simple PHP backend that queries the WMATA API

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php
+
+$DIRECTION = Array();
+$DIRECTION[2] = 'WESTBOUND';
+$DIRECTION[1] = 'EASTBOUND';
+
+$LINE = Array();
+$LINE['RD'] = 'RED';
+$LINE['BL'] = 'BLUE';
+$LINE['YL'] = 'YELLOW';
+$LINE['OR'] = 'ORANGE';
+$LINE['GR'] = 'GREEN';
+$LINE['SV'] = 'SILVER';
+$LINE['NO'] = 'OUT OF SERVICE';
+$LINE[''] = 'OUT OF SERVICE';
+
+?>
---
+++ get.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php
+<?php
+
+require 'constants.php';
+
+$key = trim(file_get_contents("KEY"));
+$station = trim(file_get_contents("STATION"));
+
+$url = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/$station";
+$headers = [
+	"Accept: application/json",
+	"api_key: $key"
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($result);
+
+// echo print_r($data, true)."\n";
+
+
+$trains = $data->Trains;
+$options = Array();
+
+for ($i = 0; $i < sizeOf($trains); $i++) {
+	if (!$options[$trains[$i]->Group]) {
+		$options[$trains[$i]->Group] = Array();
+	}
+	$optionData = Array();
+	$optionData['line'] = $trains[$i]->Line;
+	$optionData['dest'] = $trains[$i]->DestinationName;
+	$optionData['time'] = $trains[$i]->Min;
+	array_push($options[$trains[$i]->Group], $optionData);
+}
+
+// echo print_r($options, true)."\n";
+?>
+
