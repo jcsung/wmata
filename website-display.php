57af25cb7871aec84fccce184ab670dfb9f7d2c3
cmd <jeff@cmd>
2022-01-13 23:32:02 -0500

    Make more responsive

--- website-display.php
+++ website-display.php
@@ -31,6 +31,8 @@ body {
 	border-left: 1px solid black;
 }
 .container span {
+	font-size: 30px;
+	font-weight: bold;
 	text-align: left;
 }
 .container .time {
@@ -40,11 +42,32 @@ body {
 .container .accessibility {
 	width: 25%;
 }
+h1 {
+	font-size: 56px;
+}
+h2 {
+	font-size: 40px;
+}
+
+@media only screen and (max-width: 1200px) {
+	.container.outer {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;	
+	}
+	.half {
+		width: 100%;
+	}
+	.half + .half {
+		border-left: 0;
+		border-top: 1px solid black;
+	}
+}
 </style>
 </head>
 <body>
 <div class="container-centered-text"><h1>Clarendon Station</h1></div>
-<div class="container">
+<div class="container outer">
 <?php
 	for ($i = 1; $i <= 2; $i++) {
 ?>
